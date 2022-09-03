83658734ff6fc2ded4b987d69687f89e826fa6e4
[natefoxr] <[nate@foxr.xyz]>
2022-09-02 18:29:52 -0700

    Refactor quiz.js

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,36 +1,56 @@
-button = document.getElementById("quiz-button");
-img = document.getElementById("random-pokemon-img");
-userInput = document.querySelector("input");
-title = document.getElementById("quiz-text");
-score = document.getElementById("pokemon-score")
-button.addEventListener("click", startQuiz);
-let correctAnswers = 0
-let questionNumber = 0
+// Define Event Listeners and Variables used in function
+const start = document.getElementById("quiz-button");
+const next = document.getElementById("poke-next")
+const submit = document.getElementById("submit-answer")
+const img = document.getElementById("random-pokemon-img");
+const userInput = document.querySelector("input");
+const title = document.getElementById("quiz-text");
+const score = document.getElementById("pokemon-score");
+let correctAnswers = 0;
+let questionNumber = 0;
+
+start.addEventListener("click", startQuiz);
+
+// fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+//             .then((response) => response.json())
+//             .then((data) => {
+//                 let pokeImg = data.sprites.front_default;
+//                 let pokeName = data.name;
+//                 img.innerHTML = `<img src="${pokeImg}" alt="Pokemon Guessing Game Pokemon Image">`;
+//                 console.log(userInput.value)
+//                 console.log(pokeName)
+//                 if (userInput.value.toLowerCase() == pokeName.toLowerCase()) {
+//                     correctAnswers += 1;
+//                 }
+//         });
+
+function getId() {
+    let id = Math.floor(Math.random() * 1155);
+    return id
+}
+
+function check_correctness() {
+    pass
+}
 
 function startQuiz() {
-    try{
-        button.innerHTML = 'Next';
-        title.innerHTML = `${questionNumber}. Name the Pokemon!`;
-        score.classList.remove('hide')
-        userInput.classList.remove('hide')
-        score.innerHTML = `${correctAnswers}/${questionNumber}`
-        if (questionNumber < 11) {
-            let id = Math.floor(Math.random() * 1155);
-            fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    pokeImg = data.sprites.front_default;
-                    pokeName = data.name;
-                    img.innerHTML = `<img src="${pokeImg}" alt="Pokemon Guessing Game Pokemon Image">`;
-                    if (userInput.value.toLowerCase() == pokeName.toLowerCase()) {
-                        correctAnswers += 1;
-                    }
-            });
-        };
-    }
-    catch {
-        startQuiz()
-    }
-    questionNumber += 1
-};
+    start.classList.add('hide')
+    answer.classList.remove('hide')
+    title.innerHTML = `${questionNumber + 1}. Name the Pokemon!`;
+    score.classList.remove('hide')
+    userInput.classList.remove('hide')
+    score.innerHTML = `${correctAnswers}/${questionNumber}`
+    nextQuestion()
+}
+
+function nextQuestion() {
+    questionNumber++
+    let id = getId()
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                img.innerHTML = `<img src="${data.sprites.front_default}" alt="Pokemon Guessing Game Pokemon Image">`;
+        });
+}
     
+
